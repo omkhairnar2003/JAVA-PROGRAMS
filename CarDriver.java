fed2eeb71ccf67fc74e869b8ab006bd2c02ae9f5
OM SUDHIR KHAIRNAR <156451469+omkhairnar2003@users.noreply.github.com>
2025-11-24 09:33:49 +0530

    Delete CarDriver.java

--- CarDriver.java
+++
@@ -1,8 +0,0 @@
-package inheritance;
-
-public class CarDriver {
-	public static void main(String[] args) {
-		TataCar tc = new TataCar("Sedan", 1299.00, 5, "Petrol", 1000.0, "Manual", "Tigor", 1000000.00);
-		tc.showCar();
-	}
-}
